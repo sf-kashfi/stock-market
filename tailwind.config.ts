521a3dd41903faf43303bd7b55dc8997b27ce69f
Fatemeh <kashfi.sf@gmail.com>
2025-12-02 09:54:25 +0330

    remove tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,17 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,css}'],
+
   corePlugins: {
     preflight: false
   },
+
   important: '#__next',
-  plugins: [require('tailwindcss-logical'), require('./src/@core/tailwind/plugin')],
+
+  plugins: [
+    require('tailwindcss-logical')
+  ],
+
   theme: {
     extend: {}
   }
